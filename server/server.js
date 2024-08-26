c02fd971dc4359f5e0fe50caef3fcf5cbbd48651
Jitesh-v26 <jiteshvijay26@gmail.com>
2024-08-26 14:31:00 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+require("dotenv").config(); // load the environment variables into process.env
+
+const userRouter = require("./routes/userRoutes");
+const movieRouter = require("./routes/movieRoutes");
+const theatreRouter = require("./routes/theatreRoutes");
+const showRouter = require("./routes/showRoutes");
+const bookingRouter = require("./routes/bookingRoutes");
+
+const connectDb = require("./config/db");
+connectDb();
+
+/** * Routes */
+app.use((req, res, next) => {
+  console.log("request received on server", req.body, req.url);
+  next();
+});
+app.use("/api/users", userRouter);
+app.use("/api/movies", movieRouter);
+app.use("/api/theatres", theatreRouter);
+app.use("/api/shows", showRouter);
+app.use("/api/bookings", bookingRouter);
+
+app.listen(8082, () => {
+  console.log("Server is up and running on port 8082");
+});
